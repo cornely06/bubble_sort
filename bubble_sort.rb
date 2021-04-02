c18384708f4bcf85a99404325281b8c53831fb3d
Raymond Cornely <cornely06@gmail.com>
2021-04-01 21:36:03 -0400

    Format to standard

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,19 +1,25 @@
+# frozen_string_literal: true
+
 def bubble_sorter(array)
-    n = array.size - 2
-    sort = true
-    while(sort == true) do
-        sort = false
-        for i in (0..n)
-            a = array[i]
-            b = array[i + 1]
-            if a > b
-            array[i + 1] = a
-            array[i] = b
-            sort = true
-            p array
-            end
-        end
-    end
+  num = array.size - 2
+  sort = true
+  while sort == true
+    sort = false
+    sort = sorter(num, array)
+  end
+end
+
+def sorter(num, array)
+  (0..num).each do |i|
+    a = array[i]
+    b = array[i + 1]
+    next if a <= b
+
+    array[i + 1] = a
+    array[i] = b
+    p array
+    return true
+  end
 end
 
 bubble_sorter([5, 3, 7, 9, 2, 6, 8, 1])
